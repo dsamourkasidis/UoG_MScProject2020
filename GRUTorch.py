f45f5e184b3c46c4d1c58a9aff80382f421688eb
DSam <dsamourkasidis@gmail.com>
2020-06-20 21:32:21 +0300

    refactored, added classes for train, eval, checkpoints.

--- GRUTorch.py
+++ GRUTorch.py
@@ -5,9 +5,9 @@ import pandas as pd
 import os
 import time
 import numpy as np
-from torch.utils.data import TensorDataset, DataLoader
-from utility import pad_history, calculate_hit
+from utility import pad_history, calculate_hit, set_device
 from shutil import copyfile
+import train_eval
 
 
 def parse_args():
@@ -15,7 +15,7 @@ def parse_args():
 
     parser.add_argument('--epochs', type=int, default=30,
                         help='Number of max epochs.')
-    parser.add_argument('--data', nargs='?', default='data',
+    parser.add_argument('--data', nargs='?', default='data/RetailRocket',
                         help='data directory')
     parser.add_argument('--resume', type=int, default=1,
                         help='flag for resume. 1: resume training; 0: train from start')
@@ -27,7 +27,7 @@ def parse_args():
                         help='reward for the click behavior.')
     parser.add_argument('--r_buy', type=float, default=1.0,
                         help='reward for the purchase behavior.')
-    parser.add_argument('--lr', type=float, default=0.01,
+    parser.add_argument('--lr', type=float, default=0.005,
                         help='Learning rate.')
     return parser.parse_args()
 
@@ -88,186 +88,45 @@ class GRUTorch(nn.Module):
         return output
 
 
-def set_device():
-    is_cuda = torch.cuda.is_available()
-
-    # If we have a GPU available, we'll set our device to GPU. We'll use this device variable later in our code.
-    if is_cuda:
-        device = torch.device("cuda")
-    else:
-        device = torch.device("cpu")
-    return device
+class GRUEvaluator(train_eval.Evaluator):
+    def get_prediction(self, model, states, len_states, device):
+        prediction = model(states.to(device).long(), len_states.to(device).long())
+        return prediction
 
 
-def save_checkpoint(state, is_best, best_filename):
-    checkpoint = 'gru_checkpoint.pth.tar'
-    print('saved checkpoint')
-    torch.save(state, checkpoint)
-    if is_best:
-        copyfile(checkpoint, best_filename)
-        print('saved best checkpoint')
+class GRUTrainer(train_eval.Trainer):
 
+    def create_model(self):
+        gruTorch = GRUTorch(hidden_size=self.args.hidden_factor, item_num=self.item_num,
+                            state_size=self.state_size, device=self.device)
+        return gruTorch
 
-def load_from_checkpoint(args, best_filename, gru_model, optimizer, device):
-    if args.resume and os.path.isfile(best_filename):
-        print("=> loading checkpoint")
-        checkpoint = torch.load(best_filename)
-        start_epoch = checkpoint['epoch']
-        min_loss = checkpoint['min_loss']
-        gru_model.load_state_dict(checkpoint['state_dict'])
-        optimizer.load_state_dict(checkpoint['optimizer'])
+    def get_model_out(self, state, len_state):
+        out = self.model(state, len_state)
+        return out
 
-        for optim_state in optimizer.state.values():
-            for k, v in optim_state.items():
-                if torch.is_tensor(v):
-                    optim_state[k] = v.to(device)
-        print("=> loaded checkpoint. Epoch: {})".format(checkpoint['epoch']))
-    else:
-        print('no checkpoint at {}'.format(best_filename))
-        start_epoch = 0
-        min_loss = 1000
-    return start_epoch, min_loss
+    def get_evaluator(self, device, args, data_directory, state_size, item_num):
+        gru_evaluator = GRUEvaluator(device, args, data_directory, state_size, item_num)
+        return gru_evaluator
 
+    def create_optimizer(self):
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.args.lr)
+        return optimizer
 
-def prepare_dataloader(data_directory):
-    replay_buffer = pd.read_pickle(os.path.join(data_directory, 'replay_buffer.df'))
-    replay_buffer_dic = replay_buffer.to_dict()
-    states = replay_buffer_dic['state'].values()
-    states = torch.stack([torch.from_numpy(np.array(i, dtype=np.long)) for i in states]).long()
-    len_states = replay_buffer_dic['len_state'].values()
-    len_states = torch.from_numpy(np.fromiter(len_states, dtype=np.long)).long()
-    targets = replay_buffer_dic['action'].values()
-    targets = torch.from_numpy(np.fromiter(targets, dtype=np.long)).long()
-    train_data = TensorDataset(states, len_states, targets)
-    train_loader = DataLoader(train_data, shuffle=True, batch_size=args.batch_size, drop_last=True)
-    return train_loader
-
-
-def get_stats(data_dir):
-    data_statis = pd.read_pickle(
-        os.path.join(data_directory, 'data_statis.df'))  # read data statistics, includeing state_size and item_num
-    state_size = data_statis['state_size'][0]  # the length of history to define the state
-    item_num = data_statis['item_num'][0]  # total number of items
-    return state_size, item_num
-
-
-def evaluate(grumodel, data_directory, args):
-    topk = [5, 10, 15, 20]
-    reward_click = args.r_click
-    reward_buy = args.r_buy
-    eval_sessions = pd.read_pickle(os.path.join(data_directory, 'sampled_val.df'))
-    eval_ids = eval_sessions.session_id.unique()
-    groups = eval_sessions.groupby('session_id')
-    batch = 100
-    evaluated = 0
-    total_clicks = 0.0
-    total_purchase = 0.0
-    total_reward = [0, 0, 0, 0]
-    hit_clicks = [0, 0, 0, 0]
-    ndcg_clicks = [0, 0, 0, 0]
-    hit_purchase = [0, 0, 0, 0]
-    ndcg_purchase = [0, 0, 0, 0]
-    grumodel.eval()
-    while evaluated < len(eval_ids):
-        states, len_states, actions, rewards = [], [], [], []
-        for i in range(batch):
-            id = eval_ids[evaluated]
-            group = groups.get_group(id)
-            history = []
-            for index, row in group.iterrows():
-                state = list(history)
-                len_states.append(state_size if len(state) >= state_size else 1 if len(state) == 0 else len(state))
-                state = pad_history(state, state_size, item_num)
-                states.append(state)
-                action = row['item_id']
-                is_buy = row['is_buy']
-                reward = reward_buy if is_buy == 1 else reward_click
-                if is_buy == 1:
-                    total_purchase += 1.0
-                else:
-                    total_clicks += 1.0
-                actions.append(action)
-                rewards.append(reward)
-                history.append(row['item_id'])
-            evaluated += 1
-        states = torch.stack([torch.from_numpy(np.array(i, dtype=np.long)) for i in states]).long()
-        len_states = torch.from_numpy(np.fromiter(len_states, dtype=np.long)).long()
-        prediction = grumodel(states.to(device).long(), len_states.to(device).long())
-        sorted_list = np.argsort(prediction.tolist())
-        calculate_hit(sorted_list, topk, actions, rewards, reward_click, total_reward,
-                      hit_clicks, ndcg_clicks, hit_purchase, ndcg_purchase)
-    print('#############################################################')
-    print('total clicks: %d, total purchases:%d' % (total_clicks, total_purchase))
-    for i in range(len(topk)):
-        hr_click = hit_clicks[i] / total_clicks
-        hr_purchase = hit_purchase[i] / total_purchase
-        ng_click = ndcg_clicks[i] / total_clicks
-        ng_purchase = ndcg_purchase[i] / total_purchase
-        print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-        print('cumulative reward @ %d: %f' % (topk[i], total_reward[i]))
-        print('clicks hr ndcg @ %d : %f, %f' % (topk[i], hr_click, ng_click))
-        print('purchase hr and ndcg @%d : %f, %f' % (topk[i], hr_purchase, ng_purchase))
-    print('#############################################################')
+    def get_criterion(self):
+        criterion = nn.CrossEntropyLoss()
+        return criterion
 
 
 if __name__ == '__main__':
     # Network parameters
     args = parse_args()
-
     device = set_device()
     data_directory = args.data
 
-    state_size, item_num = get_stats(data_directory)
-    train_loader = prepare_dataloader(data_directory)
+    state_size, item_num = train_eval.get_stats(data_directory)
+    train_loader = train_eval.prepare_dataloader(data_directory, args.batch_size)
 
-    gruTorch = GRUTorch(hidden_size=args.hidden_factor, item_num=item_num,
-                        state_size=state_size, device=device)
+    gru_trainer = GRUTrainer('gru', args, device, state_size, item_num)
+    gru_trainer.train(train_loader)
 
-    criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(gruTorch.parameters(), lr=args.lr)
-
-    checkpoint_file = 'gru__best.pth.tar'
-    start_epoch, min_loss = load_from_checkpoint(args, checkpoint_file, gruTorch, optimizer, device)
-    gruTorch.to(device)
-
-    # Start training loop
-    epoch_times = []
-    total_step = 0
-    for epoch in range(start_epoch, args.epochs):
-        gruTorch.train()
-        start_time = time.clock()
-        avg_loss = 0.
-        counter = 0
-        for state, len_state, target in train_loader:
-            counter += 1
-            gruTorch.zero_grad()
-
-            out = gruTorch(state.to(device).long(), len_state.to(device).long())
-            loss = criterion(out, target.to(device).long())
-
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-            total_step += 1
-            if total_step % 200 == 0:
-                print("the loss in %dth batch is: %f" % (total_step, loss.item()))
-                print("Epoch {}......Batch: {}/{}....... Loss: {}".format(epoch, counter,
-                                                                          len(train_loader),
-                                                                          loss.item()))
-            if total_step % 2000 == 0:
-                evaluate(gruTorch, data_directory, args)
-                gruTorch.train()
-                is_best = loss.item() < min_loss
-                min_loss = min(min_loss, loss.item())
-                save_checkpoint({
-                    'epoch': epoch + 1,
-                    'state_dict': gruTorch.state_dict(),
-                    'min_loss': min_loss,
-                    'optimizer': optimizer.state_dict(),
-                }, is_best, checkpoint_file)
-        current_time = time.clock()
-        print("Epoch {}/{} Done, Total Loss: {}".format(epoch, args.epochs, avg_loss / len(train_loader)))
-        print("Total Time Elapsed: {} seconds".format(str(current_time - start_time)))
-        epoch_times.append(current_time - start_time)
-    print("Total Training Time: {} seconds".format(str(sum(epoch_times))))
--- utility.py
+++ utility.py
@@ -1,7 +1,6 @@
 import os
 import numpy as np
-import pandas as pd
-from collections import deque
+import torch
 
 def to_pickled_df(data_directory, **kwargs):
     for name, df in kwargs.items():
@@ -16,50 +15,6 @@ def pad_history(itemlist,length,pad_item):
         return itemlist
 
 
-def extract_axis_1(data, ind):
-    """
-    Get specified elements along the first axis of tensor.
-    :param data: Tensorflow tensor that will be subsetted.
-    :param ind: Indices to take (one for each element along axis 0 of data).
-    :return: Subsetted tensor.
-    """
-
-    batch_range = tf.range(tf.shape(data)[0])
-    indices = tf.stack([batch_range, ind], axis=1)
-    res = tf.gather_nd(data, indices)
-
-    return res
-
-
-def normalize(inputs,
-              epsilon=1e-8,
-              scope="ln",
-              reuse=None):
-    '''Applies layer normalization.
-
-    Args:
-      inputs: A tensor with 2 or more dimensions, where the first dimension has
-        `batch_size`.
-      epsilon: A floating number. A very small number for preventing ZeroDivision Error.
-      scope: Optional scope for `variable_scope`.
-      reuse: Boolean, whether to reuse the weights of a previous layer
-        by the same name.
-
-    Returns:
-      A tensor with the same shape and data dtype as `inputs`.
-    '''
-    with tf.variable_scope(scope, reuse=reuse):
-        inputs_shape = inputs.get_shape()
-        params_shape = inputs_shape[-1:]
-
-        mean, variance = tf.nn.moments(inputs, [-1], keep_dims=True)
-        beta = tf.Variable(tf.zeros(params_shape))
-        gamma = tf.Variable(tf.ones(params_shape))
-        normalized = (inputs - mean) / ((variance + epsilon) ** (.5))
-        outputs = gamma * normalized + beta
-
-    return outputs
-
 def calculate_hit(sorted_list,topk,true_items,rewards,r_click,total_reward,hit_click,ndcg_click,hit_purchase,ndcg_purchase):
     for i in range(len(topk)):
         rec_list = sorted_list[:, -topk[i]:]
@@ -73,6 +28,17 @@ def calculate_hit(sorted_list,topk,true_items,rewards,r_click,total_reward,hit_c
                 else:
                     hit_purchase[i] += 1.0
                     ndcg_purchase[i] += 1.0 / np.log2(rank + 1)
+
+
+def set_device():
+    is_cuda = torch.cuda.is_available()
+
+    # If we have a GPU available, we'll set our device to GPU. We'll use this device variable later in our code.
+    if is_cuda:
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+    return device
 
 
 # class Memory():
