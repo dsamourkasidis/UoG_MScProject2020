2359d37222e17ec81ee50116609091a034fe27bf
sreyamdasgupta <48473410+sreyamdasgupta@users.noreply.github.com>
2020-06-25 23:15:07 +0530

    Add files via upload

---
+++ SASRecModulesTorch.py
@@ -0,0 +1,234 @@
+# -*- coding: utf-8 -*-
+#/usr/bin/python2
+'''
+June 2017 by kyubyong park. 
+kbpark.linguist@gmail.com.
+https://www.github.com/kyubyong/transformer
+'''
+
+from __future__ import print_function
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+from utility_torch import normalize
+
+
+
+"""def positional_encoding(dim, sentence_length, dtype=torch.float32):
+
+    encoded_vec = np.array([pos/np.power(10000, 2*i/dim) for pos in range(sentence_length) for i in range(dim)])
+    encoded_vec[::2] = np.sin(encoded_vec[::2])
+    encoded_vec[1::2] = np.cos(encoded_vec[1::2])
+
+    return torch.tensor(encoded_vec.reshape([sentence_length, dim]), dtype=dtype)
+
+
+def embedding(inputs, 
+              vocab_size, 
+              num_units, 
+              zero_pad=True, 
+              scale=True,
+              l2_reg=0.0,
+              scope="embedding", 
+              with_t=False,
+              reuse=None):
+    '''Embeds a given tensor.
+
+    Args:
+      inputs: A `Tensor` with type `int32` or `int64` containing the ids
+         to be looked up in `lookup table`.
+      vocab_size: An int. Vocabulary size.
+      num_units: An int. Number of embedding hidden units.
+      zero_pad: A boolean. If True, all the values of the fist row (id 0)
+        should be constant zeros.
+      scale: A boolean. If True. the outputs is multiplied by sqrt num_units.
+      scope: Optional scope for `variable_scope`.
+      reuse: Boolean, whether to reuse the weights of a previous layer
+        by the same name.
+
+    Returns:
+      A `Tensor` with one more rank than inputs's. The last dimensionality
+        should be `num_units`.
+        
+    For example,
+    
+    ```
+    import tensorflow as tf
+    
+    inputs = tf.to_int32(tf.reshape(tf.range(2*3), (2, 3)))
+    outputs = embedding(inputs, 6, 2, zero_pad=True)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        print sess.run(outputs)
+    >>
+    [[[ 0.          0.        ]
+      [ 0.09754146  0.67385566]
+      [ 0.37864095 -0.35689294]]
+
+     [[-1.01329422 -1.09939694]
+      [ 0.7521342   0.38203377]
+      [-0.04973143 -0.06210355]]]
+    ```
+    
+    ```
+    import tensorflow as tf
+    
+    inputs = tf.to_int32(tf.reshape(tf.range(2*3), (2, 3)))
+    outputs = embedding(inputs, 6, 2, zero_pad=False)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        print sess.run(outputs)
+    >>
+    [[[-0.19172323 -0.39159766]
+      [-0.43212751 -0.66207761]
+      [ 1.03452027 -0.26704335]]
+
+     [[-0.11634696 -0.35983452]
+      [ 0.50208133  0.53509563]
+      [ 1.22204471 -0.96587461]]]    
+    ```    
+    '''
+    with tf.variable_scope(scope, reuse=reuse):
+        lookup_table = tf.get_variable('lookup_table',
+                                       dtype=tf.float32,
+                                       shape=[vocab_size, num_units],
+                                       #initializer=tf.contrib.layers.xavier_initializer(),
+                                       regularizer=tf.contrib.layers.l2_regularizer(l2_reg))
+        if zero_pad:
+            lookup_table = tf.concat((tf.zeros(shape=[1, num_units]),
+                                      lookup_table[1:, :]), 0)
+        outputs = tf.nn.embedding_lookup(lookup_table, inputs)
+        
+        if scale:
+            outputs = outputs * (num_units ** 0.5) 
+    if with_t: return outputs,lookup_table
+    else: return outputs"""
+
+
+def multihead_attention(queries,
+                        keys,
+                        num_units=None, 
+                        num_heads=8, 
+                        dropout_rate=0,
+                        causality=False,
+                        with_qk=False):
+    '''Applies multihead attention.
+    
+    Args:
+      queries: A 3d tensor with shape of [N, T_q, C_q].
+      keys: A 3d tensor with shape of [N, T_k, C_k].
+      num_units: A scalar. Attention size.
+      dropout_rate: A floating point number.
+      is_training: Boolean. Controller of mechanism for dropout.
+      causality: Boolean. If true, units that reference the future are masked. 
+      num_heads: An int. Number of heads.
+      scope: Optional scope for `variable_scope`.
+      reuse: Boolean, whether to reuse the weights of a previous layer
+        by the same name.
+        
+    Returns
+      A 3d tensor with shape of (N, T_q, C)  
+    '''
+    # Set the fall back option for num_units
+    if num_units is None:
+        num_units = queries.size().as_list[-1]
+    # Linear projections
+    Q = nn.Linear(queries, num_units) # (N, T_q, C)
+    K = nn.Linear(keys, num_units) # (N, T_k, C)
+    V = nn.Linear(keys, num_units) # (N, T_k, C)
+    
+    # Split and concat
+    q_split = Q.size(2)/num_heads
+    k_split = K.size(2)/num_heads
+    v_split = V.size(2)/num_heads
+    Q_ = torch.cat(torch.split(Q, q_split, dim=2), dim=0) # (h*N, T_q, C/h) 
+    K_ = torch.cat(torch.split(K, k_split, dim=2), dim=0) # (h*N, T_k, C/h) 
+    V_ = torch.cat(torch.split(V, v_split, dim=2), dim=0) # (h*N, T_k, C/h) 
+
+    # Multiplication
+    outputs = torch.matmul(Q_, K.permute[0, 2, 1]) # (h*N, T_q, T_k)
+        
+    # Scale
+    outputs = outputs / (K_.get_shape().as_list()[-1] ** 0.5)
+        
+    # Key Masking
+    key_masks = torch.sign(torch.abs(torch.sum(keys,-1))) # (N, T_k)
+    key_masks = torch.cat(num_heads*[key_masks]) # (h*N, T_k)
+    key_masks = torch.cat(queries.size(1)*[key_masks.unsqueeze(1)], dim=1) # (h*N, T_q, T_k)
+        
+    paddings = torch.ones_like(outputs)*(-2**32+1)
+    outputs = torch.where(torch.eq(key_masks, 0), paddings, outputs) # (h*N, T_q, T_k)
+  
+    # Causality = Future blinding
+    if causality:
+        diag_vals = torch.ones_like(outputs[0, :, :]) # (T_q, T_k)
+        tril = torch.tril(diag_vals) # (T_q, T_k)
+        masks = torch.cat(outputs.size(0)*[tril.unsqueeze(0)]) # (h*N, T_q, T_k)
+   
+        paddings = torch.ones_like(masks)*(-2**32+1)
+        outputs = torch.where(torch.eq(masks, 0), paddings, outputs) # (h*N, T_q, T_k)
+  
+    # Activation
+    outputs = nn.Softmax(outputs) # (h*N, T_q, T_k)
+         
+    # Query Masking
+    query_masks = torch.sign(torch.abs(torch.sum(queries,-1))) # (N, T_q)
+    query_masks = torch.cat(num_heads*[query_masks]) # (h*N, T_q)
+    query_masks = torch.cat(keys.size(1)*[query_masks.unsqueeze(-1)], dim=2) # (h*N, T_q, T_k)
+    outputs *= query_masks # broadcasting. (N, T_q, C)
+    
+    # Dropouts
+    dropout = nn.Dropout(dropout_rate)
+    outputs = dropout(outputs)
+               
+    # Weighted sum
+    outputs = torch.matmul(outputs, V_) # ( h*N, T_q, C/h)
+        
+    # Restore shape
+    o_split = outputs.size(0)/num_heads
+    outputs = torch.cat(torch.split(outputs, o_split, dim=0), dim=2 ) # (N, T_q, C)
+              
+    # Residual connection
+    outputs += queries
+              
+    # Normalize
+    #outputs = normalize(outputs) # (N, T_q, C)
+ 
+    if with_qk: return Q,K
+    else: return outputs
+
+class feedforward(nn.Module):
+    """# Inner layer
+    params = {"inputs": inputs, "filters": num_units[0], "kernel_size": 1,
+              "activation": F.relu, "use_bias": True}
+    outputs = tf.layers.conv1d(**params)
+    outputs = tf.layers.dropout(outputs, rate=dropout_rate, training=tf.convert_to_tensor(is_training))
+    # Readout layer
+    params = {"inputs": outputs, "filters": num_units[1], "kernel_size": 1,
+              "activation": None, "use_bias": True}
+    outputs = tf.layers.conv1d(**params)
+    outputs = tf.layers.dropout(outputs, rate=dropout_rate, training=tf.convert_to_tensor(is_training))
+        
+        # Residual connection
+    outputs += inputs
+        
+        # Normalize
+        #outputs = normalize(outputs)
+    
+    return outputs"""
+    def __init__(self, num_units=[2048, 512], dropout_rate=0.2):
+        super(feedforward,self).__init__()
+        self.inner_cnn = nn.Conv1d(1,num_units[0],1)
+        self.readout_cnn = nn.Conv1d(num_units[0],num_units[1],1)
+        self.dropout = nn.Dropout(dropout_rate)
+        
+    def forward (self,inputs):
+        x = F.relu(self.inner_cnn(inputs))
+        x = self.dropout(x)
+        x = self.readout_cnn(x)
+        x = self.dropout(x)
+        x += inputs
+        
+        return x
+
---
+++ SASRecTorch.py
@@ -0,0 +1,165 @@
+import torch
+import torch.nn as nn
+import argparse
+from utility_torch import extract_axis_1, normalize, set_device
+from SASRecModulesTorch import multihead_attention,feedforward
+import train_eval
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Run SASRec.")
+
+    parser.add_argument('--mode', default='train',
+                        help='Train or test the model. "train" or "test"')
+    parser.add_argument('--epochs', type=int, default=4,
+                        help='Number of max epochs.')
+    parser.add_argument('--data', nargs='?', default='data',
+                        help='data directory')
+    parser.add_argument('--resume', type=int, default=1,
+                        help='flag for resume. 1: resume training; 0: train from start')
+    parser.add_argument('--batch_size', type=int, default=256,
+                        help='Batch size.')
+    parser.add_argument('--hidden_factor', type=int, default=64,
+                        help='Number of hidden factors, i.e., embedding size.')
+    parser.add_argument('--r_click', type=float, default=0.2,
+                        help='reward for the click behavior.')
+    parser.add_argument('--r_buy', type=float, default=1.0,
+                        help='reward for the purchase behavior.')
+    parser.add_argument('--lr', type=float, default=0.01,
+                        help='Learning rate.')
+    parser.add_argument('--num_heads', default=1, type=int)
+    parser.add_argument('--num_blocks', default=1, type=int)
+    parser.add_argument('--dropout_rate', default=0.1, type=float)
+    return parser.parse_args()
+
+
+class SASRecTorch(nn.Module):
+    def __init__(self, hidden_size,item_num,state_size,device,num_blocks,num_heads,dropout_rate):
+        self.hidden_size = hidden_size
+        self.item_num = int(item_num)
+        self.state_size = state_size
+        self.device = torch.device("cpu")
+        self.num_blocks = num_blocks
+        self.num_heads = num_heads
+        self.dropout_rate=dropout_rate
+        self.item_embeddings = nn.Embedding(
+            num_embeddings=self.item_num+1,
+            embedding_dim=self.hidden_size,
+        )
+        self.pos_embeddings = nn.Embedding(
+            num_embeddings=self.state_size,
+            embedding_dim=self.hidden_size,
+        )
+        
+        # init embedding
+        nn.init.normal_(self.item_embeddings.weight, 0, 0.01)
+        nn.init.normal_(self.pos_embeddings.weight, 0, 0.01)
+        
+        #Multihead Attention Layer
+        #x`self.multihead_attention = multihead_attention()
+        
+        #Feedforward Layer
+        self.feedforward = feedforward()
+        
+        #dropout
+        self.dropout = nn.Dropout(self.dropout_rate)
+        
+        #Fully connected Layer
+        #self.fc1 = nn.Linear()
+        
+    def forward(self, inputs, inputs_lengths):
+        input_emb = self.item_embeddings(inputs)
+        pos_emb_input = torch.cat(inputs.size(0)*[torch.range(start=0,end=inputs.size(1)-1).unsqueeze(0)])
+        pos_emb_input = pos_emb_input.long()
+        pos_emb = self.pos_embeddings(pos_emb_input)
+        x = input_emb+pos_emb
+            
+        x = self.dropout(x)
+            
+        mask = torch.ne(inputs, self.item_num).float().unsqueeze(-1)
+        x *= mask
+            
+        for i in range (self.num_blocks):
+            x = multihead_attention(queries=normalize(x),keys=x,
+                                             num_units=self.hidden_size,
+                                             num_heads=self.num_heads,
+                                             dropout_rate=self.dropout_rate,
+                                             causality=True)
+        x = self.feedforward(normalize(x),num_units=[self.hidden_size,self.hidden_size],
+                                     dropout_rate=self.dropout_rate)
+        x *= mask
+            
+        x = normalize(x)
+        out = extract_axis_1(x,inputs_lengths-1)
+        out = nn.Linear(out,self.item_num)
+            
+        return out
+            
+                
+        
+class SASRecEvaluator(train_eval.Evaluator):
+    def get_prediction(self, model, states, len_states, device):
+        prediction = model(states.to(device).long(), len_states.to(device).long())
+        return prediction
+
+
+class SASRecTrainer(train_eval.Trainer):
+
+    def create_model(self):
+        sasrecTorch = SASRecTorch(hidden_size=self.args.hidden_factor, item_num=self.item_num,
+                            state_size=self.state_size, device=self.device,num_blocks=self.args.num_blocks,
+                            num_heads=self.args.num_heads,dropout_rate=self.args.dropout_rate)
+        return sasrecTorch
+
+    def get_model_out(self, state, len_state):
+        out = self.model(state, len_state)
+        return out
+
+    def get_evaluator(self, device, args, data_directory, state_size, item_num):
+        sasrec_evaluator = SASRecEvaluator(device, args, data_directory, state_size, item_num)
+        return sasrec_evaluator
+
+    def create_optimizer(self):
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.args.lr)
+        return optimizer
+
+    def get_criterion(self):
+        criterion = nn.CrossEntropyLoss()
+        return criterion
+
+
+TRAIN = 'train'
+TEST = 'test'
+
+
+def train_model(args, device, state_size, item_num):
+    sasrec_trainer = SASRecTrainer('caser_RC15', args, device, state_size, item_num)
+    sasrec_trainer.train(train_loader)
+
+
+def test_model(device, args, data_directory, state_size, item_num):
+    sasrecTorch = SASRecTorch(hidden_size=args.hidden_factor, item_num=item_num,
+                        state_size=state_size, device=device,num_blocks=args.num_blocks,
+                            num_heads=args.num_heads,dropout_rate=args.dropout_rate)
+    checkpoint_handler = train_eval.CheckpointHandler('sasrec_RC15', device)
+    optimizer = torch.optim.Adam(sasrecTorch.parameters(), lr=args.lr)
+    _, _ = checkpoint_handler.load_from_checkpoint(True, sasrecTorch, optimizer)
+    sasrecTorch.to(device)
+
+    sasrec_evaluator = SASRecEvaluator(device, args, data_directory, state_size, item_num)
+    sasrec_evaluator.evaluate(sasrecTorch, 'test')
+
+
+if __name__ == '__main__':
+    # Network parameters
+    args = parse_args()
+    device = set_device()
+    data_directory = args.data
+
+    state_size, item_num = train_eval.get_stats(data_directory)
+    train_loader = train_eval.prepare_dataloader(data_directory, args.batch_size)
+
+    if args.mode.lower() == TRAIN:
+        train_model(args, device, state_size, item_num)
+    else:
+        test_model(device, args, data_directory, state_size, item_num)
